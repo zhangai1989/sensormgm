ac66f9b64db259af388363914e20efbd600bffb2
ai.zhang <ai.zhang@changhong.com>
2019-07-12 18:18:39 +0800

    Merge branch 'dev' of https://github.com/zhangai1989/sensormgm into dev

--- src/main/java/com/bumt/sensormgm/controller/TAreaController.java
+++ src/main/java/com/bumt/sensormgm/controller/TAreaController.java
@@ -14,6 +14,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -107,6 +109,37 @@ public class TAreaController  extends BaseController<TArea>{
 	@ApiImplicitParam(paramType="query", name = "areaId", value = "区域id", required = true, dataType = "String")
 	public Object getAreaList(HttpSession session){
 		return new ResultUtil<>().setData(service.getAreaList(session));
+	}
+
+
+	/**
+	 *@author : zhangai
+	 *@Date : 11:31 2019/6/24
+	 * @Parm: pageNum第几页  pageSize一页的数量 因为存在分页信息的参数，无法使用对象接收
+	 *@描述：获取单页信息
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/getAreaList2", produces = {"application/json;charset=UTF-8"})
+	public Object getAreaList2(@RequestBody Map<String,Object> entity,HttpSession session){
+
+		int pageNum = Integer.parseInt(entity.get("pageNum").toString());
+		int pageSize = Integer.parseInt(entity.get("pageSize").toString());
+		Pageable pageable= PageRequest.of((pageNum-1),pageSize,changeSort());
+
+		Specification<TArea> querySpecification = (root, criteriaQuery,criteriaBuilder)-> {
+			List<Predicate> list = new ArrayList<Predicate>();
+			TUser tUser = (TUser) session.getAttribute("user");
+			if(tUser!=null){
+				if(tUser.getLevel()==3){
+					list.add(criteriaBuilder.equal(root.get("id").as(String.class),tUser.getAreaId()));
+				}
+			}
+			list.add(criteriaBuilder.equal(root.get("level").as(String.class),2));
+			list.add(criteriaBuilder.equal(root.get("deleteFlag").as(String.class),0));
+			return criteriaBuilder.and(list.toArray(new Predicate[list.size()]));
+		};
+
+		return new ResultUtil<>().setData(getService().getPageListByCondition(querySpecification,pageable));
 	}
 
 	@ResponseBody
